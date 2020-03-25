f655bd373de7f7e11198e2396da3779819714b31
kdships <kdships@gmail.com>
2020-03-25 01:25:49 -0500

    Update selfservice.ps1
    Separated create Teams block from add owner and applied Try/Catch on both.

--- selfservice.ps1
+++ selfservice.ps1
@@ -145,19 +145,16 @@ Foreach($word in $Array)
     $Check2 = $Null
     $Check3 = $Null
     $Check4 = $Null
-    $Check5 = $Null
-    $Check1 = Get-UnifiedGroup -Identity $TeamsName
-    Start-Sleep -Seconds 2
-    $Check2 = Get-UnifiedGroup -Identity ($TeamsName + '@XXXXX.onmicrosoft.com')
-    Start-Sleep -Seconds 2
-    $Check3 = Get-UnifiedGroup -Identity $Name5
-    Start-Sleep -Seconds 2
-    $Check4 = Get-UnifiedGroup -Identity ($Name5 + '@XXXXX.onmicrosoft.com')
-    Start-Sleep -Seconds 2
-    $Check5 = Get-UnifiedGroup -Identity "$Name"
-    if (($Check1 -eq $Null) -and ($Check2 -eq $Null) -and ($Check3 -eq $Null) -and ($Check4 -eq $Null) -and ($Check5 -eq $Null))
+    $Check1 = Get-Recipient -Identity $TeamsName
+    Start-Sleep -Seconds 3
+    $Check2 = Get-Recipient -Identity ($TeamsName + '@Company.com')
+    Start-Sleep -Seconds 3
+    $Check3 = Get-Recipient -Identity ($TeamsName + '@Company.onmicrosoft.com')
+    Start-Sleep -Seconds 3
+    $Check4 = Get-Recipient -Identity "$Name"
+    if (($Check1 -eq $Null) -and ($Check2 -eq $Null) -and ($Check3 -eq $Null) -and ($Check4 -eq $Null))
     {
-    Start-Sleep -Seconds 2
+    Start-Sleep -Seconds 10
                     If($VisibilityType -eq "Private - Only Team owners can add members")
                     {
                 $VisibilityType = "Private"
@@ -167,74 +164,65 @@ Foreach($word in $Array)
                 $VisibilityType = "Public"
                 }
         Try {
-            #Create Team and add the requestor as owner
+            #Create Team
             $ReadyTeam = $Null
             $RunCheck = 0
-            Start-Sleep -Seconds 3
-            $ReadyTeam = New-Team -MailNicknames $TeamsName -displayname $Name -Visibility $VisibilityType -Description $Description
+            $ReadyTeam = New-Team -MailNickname $TeamsName -displayname $Name -Visibility $VisibilityType -Description $Description
             $RunCheck = 1
             Start-Sleep -Seconds 10
             }
         Catch {
-            If($Runcheck -eq 0)
-              {
-            Try{
-            #Create Team and add the requestor as owner
-            $ReadyTeam = $Null
-            Start-Sleep -Seconds 10
-            $ReadyTeam = New-Team -MailNickname $TeamsName -displayname $Name -Visibility $VisibilityType -Description $Description
-            $RunCheck = 3
-            Start-Sleep -Seconds 10
-            }
-            Catch{
-            #Email status of the request to the requestor, email request details to IT team and delete request from SharePoint
-            $UriT = $Null
-            $StatusValue = $Null
-            $UriT = "Insert Invoke PowerAutomate URL Path HERE"
-            [int]$StatusValue = "3"
-                    $BodyT = [ordered] @{
-                    EntryID = $EntryID
-                    email   = $RequestorsEmail
-                    checkvalue = $StatusValue
-                    subject = "Your Team($Name) request is pending"
-                    bodyvalue    = "Your Team($Name) provisioning request is incomplete. A member of the IT team will be in touch as soon as it is rectified."
-                    firstname = "Hi $GivenName"
-                  } | ConvertTo-Json -Depth 10
-            Invoke-RestMethod -Method POST -Uri $UriT -Body $BodyT -ContentType application/json
-            $Subject = "Action Required: Failed to create $Name Team"
-            $Body = $Null
-            $Body = "Hi Team,
+                #Email status of the request to the requestor, email request details to IT team and delete request from SharePoint
+                $UriT = $Null
+                $StatusValue = $Null
+                $UriT = "https://prod-121.REGION.logic.azure.com:443/workflows/FLOWPATHID"
+                [int]$StatusValue = "3"
+                        $BodyT = [ordered] @{
+                        EntryID = $EntryID
+                        email   = $RequestorsEmail
+                        checkvalue = $StatusValue
+                        subject = "Your Team request is pending - $Name"
+                        bodyvalue    = "Your Team provisioning request is pending. A member of the IT team will be in touch as soon as it is rectified."
+                        firstname = "Hi $GivenName"
+                      } | ConvertTo-Json -Depth 10
+                Invoke-RestMethod -Method POST -Uri $UriT -Body $BodyT -ContentType application/json
+                $Subject = "Action Required: Failed to create Team - $Name"
+                $Body = $Null
+                $Body = "Hi Team,
     
-            MS Teams provisioning self-service app failed to complete this request. Pending action can be found in the error details below. Using the details below, please kindly retry creatng Teams through the self-service app.
+                MS Teams provisioning self-service app failed to create this Team. Using the details below, please kindly retry creatng the Team through the self-service app and update the ownership. Remember to remove yourself as the owner.
     
-            Team Display Name: $Name
-            Team NickName/Alias: $TeamsName
-            Visibility preference: $VisibilityType
-            Description: $Description
-            Requestor's Email Address: $RequestorsEmail
-            Requestor's UPN: $UPN
-            Reason for the failure: $_
+                Self-Service App Link: https://apps.powerapps.com/play/APPID
+                Team Display Name: $Name
+                Team NickName/Alias: $TeamsName
+                Visibility preference: $VisibilityType
+                Description: $Description
+                Requestor's Email Address: $RequestorsEmail
+                Requestor's UPN: $UPN
+                Reason for the failure: $_
     
-            Sent by a trigger for 'TeamsProvisioningSelfServiceApp' task on server: XXXXXXX"
-            Send-MailMessage -From "XXXXXXX" -to "XXXXXXX" -Subject $Subject -Body $Body -priority High -SmtpServer XXXXX
-            Write-host "Failed to complete processing Team for $UPN. Reason: $_" -ForegroundColor Red
+                Sent by a trigger for 'TeamsProvisioningSelfServiceApp' task"
+                Send-MailMessage -From "TeamsSelfServiceApp@Company.com" -to "Company@ticketing.com" -Subject $Subject -Body $Body -priority normal -SmtpServer email.Company.com
                 }
-            Finally{
-                If($RunCheck -eq 3)
-                {
-                    Start-Sleep -Seconds 7
-                    Add-TeamUser -GroupId $ReadyTeam.GroupId -User $UPN -Role Owner
-                    #Email status of the request to the requestor, email request details to IT team and delete request from SharePoint
+        If($RunCheck -eq 1)
+        {       #Add the requestor as owner of the Team and remove the service account
+                Start-Sleep -Seconds 13
+                Try{
+                $RunCheckB = 0
+                Add-TeamUser -GroupId $ReadyTeam.GroupId -User $UPN -Role Owner
+                $RunCheckB = 1
+                Start-Sleep -Seconds 3
+                #Email status of the request to the requestor, email request details to IT team and delete request from SharePoint
                     $UriT = $Null
                     $StatusValue = $Null
-                    $UriT = "Insert Invoke PowerAutomate URL Path HERE"
+                    $UriT = "https://prod-121.REGION.logic.azure.com:443/workflows/FLOWPATHID"
                     [int]$StatusValue = "2"
                             $BodyT = [ordered] @{
                             EntryID = $EntryID
                             email   = $RequestorsEmail
                             checkvalue = $StatusValue
                             subject = "[$Name] Team has been successfully created"
-                            bodyvalue    = "[$Name] Team has been created. It should now be visible within your Teams client. 
+                            bodyvalue    = "[$Name] Team has been created. Shortly, it will be visible within your Teams client. If you do not find it, please scroll to the bottom of your Teams list.
                     
                             Team Name: $Name. 
             
@@ -245,32 +233,20 @@ Foreach($word in $Array)
                           } | ConvertTo-Json -Depth 10
                     Invoke-RestMethod -Method POST -Uri $UriT -Body $BodyT -ContentType application/json
                     Write-host "$TeamsName was successfully created for $UPN" -ForegroundColor Green
-                    Send-MailMessage -From $RequestorsEmail -to "XXXXX" -Subject "New Teams Request by $RequestorsEmail" -Body "$TeamsName - New Teams Request by $RequestorsEmail" -priority Low -SmtpServer XXXXXX
-                Start-Sleep -Seconds 7
-                Remove-TeamUser -GroupId $ReadyTeam.GroupId -User <Insert service account here> -Role Owner
-                Start-Sleep -Seconds 2
-                Remove-TeamUser -GroupId $ReadyTeam.GroupId -User <insert service account here>
+                    Send-MailMessage -From "TeamsSelfServiceApp@Company.com" -to "kdships@Company.com" -Subject "New Teams Request by $UPN" -Body "$TeamsName - New Teams Request by $RequestorsEmail | $UPN" -priority Low -SmtpServer email.Company.com
                 }
-                }
-              }
-            }
-            Finally
-            {
-                If($RunCheck -eq 1)
-                {
-                    Start-Sleep -Seconds 10
-                    Add-TeamUser -GroupId $ReadyTeam.GroupId -User $UPN -Role Owner
-                    #Email status of the request to the requestor, email request details to IT team and delete request from SharePoint
+                Catch{
+                #Email status of the request to the requestor, email request details to IT team and delete request from SharePoint
                     $UriT = $Null
                     $StatusValue = $Null
-                    $UriT = "<Insert Invoke PowerAutomate URL Path HERE>"
+                    $UriT = "https://prod-121.REGION.logic.azure.com:443/workflows/FLOWPATHID"
                     [int]$StatusValue = "2"
                             $BodyT = [ordered] @{
                             EntryID = $EntryID
                             email   = $RequestorsEmail
                             checkvalue = $StatusValue
-                            subject = "[$Name] Team has been successfully created"
-                            bodyvalue    = "[$Name] Team has been created. It should now be visible within your Teams client. 
+                            subject = "[$Name] Team has been successfully created but pending one final step"
+                            bodyvalue    = "[$Name] Team has been created. We are applying finishing touches and would let you know as soon as it is ready.
                     
                             Team Name: $Name. 
             
@@ -280,38 +256,80 @@ Foreach($word in $Array)
                             firstname = "Hi $GivenName"
                           } | ConvertTo-Json -Depth 10
                     Invoke-RestMethod -Method POST -Uri $UriT -Body $BodyT -ContentType application/json
-                    Write-host "$TeamsName was successfully created for $UPN" -ForegroundColor Green
-                    Send-MailMessage -From $RequestorsEmail -to "XXXXXXXXXXXXX" -Subject "New Teams Request by $RequestorsEmail" -Body "$TeamsName - New Teams Request by $RequestorsEmail" -priority Low -SmtpServer XXXXXXX
-                    Start-Sleep -Seconds 7
-                    Remove-TeamUser -GroupId $ReadyTeam.GroupId -User <insert service account here> -Role Owner
-                    Start-Sleep -Seconds 2
-                    Remove-TeamUser -GroupId $ReadyTeam.GroupId -User <insert service account here>
+                $Subject = "Action Required: Failed to create Team owner"
+                $Body = $Null
+                $Body = "Hi Team,
+    
+                MS Teams provisioning self-service app failed to add the owner to the team. Using the details below, kindly add the owner through Teams admin portal or through the Client. Please remember to remove yourself and Collab service account from the Team.
+    
+                Team Display Name: $Name
+                Requestor's Email Address: $RequestorsEmail
+                Requestor's UPN: $UPN
+                Reason for the failure: $_
+    
+                Sent by a trigger for 'TeamsProvisioningSelfServiceApp' task"
+                Send-MailMessage -From "TeamsSelfServiceApp@Company.com" -to "Company@ticketing.com" -Subject $Subject -Body $Body -priority normal -SmtpServer email.Company.com
+                Send-MailMessage -From "TeamsSelfServiceApp@Company.com" -to "kdships@Company.com" -Subject $Subject -Body $Body -priority High -SmtpServer email.Company.com
+                Write-host "Adding owner to $Name failed - Owner: $UPN - Reason: $_" -ForegroundColor Red
                 }
-            }
+                Finally{
+                If($RunCheckB -eq 1){
+                Start-Sleep -Seconds 3
+                Remove-TeamUser -GroupId $ReadyTeam.GroupId -User serviceaccount@Company.com -Role Owner
+                Start-Sleep -Seconds 3
+                Remove-TeamUser -GroupId $ReadyTeam.GroupId -User serviceaccount@Company.com}
+                }
+        }
+        elseif($RunCheck -eq 0)
+        {
+        Send-MailMessage -From "TeamsSelfServiceApp@Company.com" -to "kdships@Company.com" -Subject $Subject -Body $Body -priority High -SmtpServer email.Company.com
+        Write-host "Failed to complete processing Team for $UPN. Reason: $_" -ForegroundColor Red
+        }             
     }
-    elseif (($Check1 -ne $Null) -or ($Check2 -ne $Null) -or ($Check3 -ne $Null) -or ($Check4 -ne $Null) -or ($Check5 -ne $Null))
+    elseif (($Check1 -ne $Null) -or ($Check2 -ne $Null) -or ($Check3 -ne $Null) -or ($Check4 -ne $Null))
     {
     #Check who owns the team
-    $Owners = $Null
+    $Owners = 0
     $Owners = (Get-UnifiedGroup -Identity "$Name").ManagedBy
-    #Email status of the request to the requestor and delete request from SharePoint
-    $UriT = $Null
-    $StatusValue = $Null
-    $UriT = "<Insert Invoke PowerAutomate URL Path HERE>"
-    [int]$StatusValue = "4"
-            $BodyT = [ordered] @{
-            EntryID = $EntryID
-            email   = $RequestorsEmail
-            checkvalue = $StatusValue
-            subject = "Failed to create $Name Team - Reason: Name Conflict"
-            bodyvalue    = "
-            Your Team($Name) request failed due to a conflict with an existing Team or Group. The name you provided is already in use by: $Owners. 
+        If($Owners.count -gt 0)
+                {
+                #Email status of the request to the requestor and delete request from SharePoint
+                $UriT = $Null
+                $StatusValue = $Null
+                $UriT = "https://prod-121.REGION.logic.azure.com:443/workflows/FLOWPATHID"
+                [int]$StatusValue = "4"
+                        $BodyT = [ordered] @{
+                        EntryID = $EntryID
+                        email   = $RequestorsEmail
+                        checkvalue = $StatusValue
+                        subject = "Failed to create $Name Team - Reason: Name Conflict"
+                        bodyvalue    = "
+                        Your Team request failed due to a conflict with an existing Team or Group. The name you provided is already in use by: $Owners. 
             
-            Please change the name and try again."
-            firstname = "Hi $GivenName"
-          } | ConvertTo-Json -Depth 10
-    Invoke-RestMethod -Method POST -Uri $UriT -Body $BodyT -ContentType application/json
-    Write-host "Failed to create Team due to a name conflict" -ForegroundColor Red
+                        Please change the name and try again."
+                        firstname = "Hi $GivenName"
+                      } | ConvertTo-Json -Depth 10
+                Invoke-RestMethod -Method POST -Uri $UriT -Body $BodyT -ContentType application/json
+                }
+                else
+                {
+                #Email status of the request to the requestor and delete request from SharePoint
+                $UriT = $Null
+                $StatusValue = $Null
+                $UriT = "https://prod-121.REGION.logic.azure.com:443/workflows/FLOWPATHID"
+                [int]$StatusValue = "4"
+                        $BodyT = [ordered] @{
+                        EntryID = $EntryID
+                        email   = $RequestorsEmail
+                        checkvalue = $StatusValue
+                        subject = "Failed to create $Name Team - Reason: Name Conflict"
+                        bodyvalue    = "
+                        Your Team request failed due to a conflict with an existing object. Please change the name and try again."
+                        firstname = "Hi $GivenName"
+                      } | ConvertTo-Json -Depth 10
+                Invoke-RestMethod -Method POST -Uri $UriT -Body $BodyT -ContentType application/json
+                }
+    Write-host "Failed to create Team due to a name conflict - $Name" -ForegroundColor Red
     }
 }
 }
